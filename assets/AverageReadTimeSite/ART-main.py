ddfd4a689325016341816697ff0674d986d0b1b5
Hakob-Harutyunyan <hakob.h8.y@tumo.org>
2023-10-01 00:15:59 +0400

    fix : predict

--- assets/AverageReadTimeSite/ART-main.py
+++ assets/AverageReadTimeSite/ART-main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import keras_nlp
+from flask import Flask, render_template, request
 
 tokenizer = keras_nlp.models.BertTokenizer.from_preset(
     "bert_base_en_uncased",
@@ -16,4 +17,18 @@ def eager_to_tensor_function(eager_tensor):
 
 def predict(text):
     return model.predict(eager_to_tensor_function(tokenizer(text)))\
-    
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('AverageReadTimeModel.html')
+
+
+@app.route('/on_button_click', methods=['POST'])
+def on_button_click():
+    # Execute your python function here.
+    link = request.form.get('link')
+    print(f'Button clicked: {link}')
+    return 'Button Click'
